f1db5da2a7d403bcb9f8ca4f052670d04e419f20
liaas039 <wedserty@yahoo.com.tw>
2022-06-19 03:27:16 +0800

    1

---
+++ assets/js/member_tabcontrol.js
@@ -0,0 +1,15 @@
+window.onload=open_memberfun;
+function open_memberfun(evt, mem_tab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(mem_tab).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+ 
